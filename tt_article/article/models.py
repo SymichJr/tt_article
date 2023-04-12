ef1c62c1df340652a93ba3d444e6521438565826
Kirill Maksimov <kamaksimov1410@gmail.com>
2023-04-12 22:20:30 +0300

    Created article model

--- tt_article/article/models.py
+++ tt_article/article/models.py
@@ -1,4 +1,21 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    pub_date = models.DateTimeField('Pub date', auto_now_add=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='articles'
+    )
+    is_public = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-id']
+    
+    def __str__(self):
+        return self.title
+
+
